a42dd75047c766a95b8194b9bba450f858fb1b8f
hadd4way <Wazeronid@gmail.com>
2023-08-23 21:27:09 +0400

    content render середина

--- scripts/app.js
+++ scripts/app.js
@@ -5,7 +5,20 @@ const HABBIT_KEY = 'HABBIT_KEY';
 let habbits = [];
 
 const page ={
-    menu: document.querySelector('.menu__list')
+    menu: document.querySelector('.menu__list'),
+    header: {
+        h1: document.querySelector('h1'),
+        progressPercent: document.querySelector('.progress__percent'),
+        progressCoverBar: document.querySelector('.progress__bar__cover')
+    },
+    content: {
+        habbit: document.querySelector('.habbit'),
+        habbitList: document.querySelector('.habbit__list'),
+        habbitDay: document.querySelector('.habbit__day'),
+        habbitComment: document.querySelector('.habbit__comment'),
+        habbitDeleteButton: document.querySelector('.habbit__delete'),
+        main: document.querySelector('main')
+    }
 }
 
         // util // 
@@ -20,6 +33,10 @@ function loadData() {
 
 function saveData(){
     localStorage.setItem(HABBIT_KEY,JSON.stringify(habbits));
+}
+
+function addDay(activeHabbit){
+    
 }
         // render //
 
@@ -49,11 +66,63 @@ function rerenderMenu(activeHabbit){
             existed.classList.remove('menu__item_active')
         }
     }
+    let addElement = document.createElement('div');
+    addElement.classList.add('habbit');
+    addElement.innerHTML = `
+    <div class="habbit__day">День ${Number(activeHabbit.days.length)+1}</div>
+    <form class="habbit__form" action = "#">
+        <input type="text" class="input__icon" placeholder="Комментарий">
+        <img src="/images/comment.svg" alt="commentIcon" class="input_icon">
+        <button class="habbit__form__button" type='button'>Готово</button>
+    </form>
+    `
+    addElement.querySelector('.habbit__form__button').addEventListener('click', () =>{
+        activeHabbit.days.push({comment: addElement.querySelector('.input__icon').value})
+        rerender(activeHabbit.id);
+    })
+    page.content.main.appendChild(addElement);
 }
 
+function rerenderHeader(activeHabbit){
+    if(!activeHabbit){
+        return;
+    }
+    page.header.h1.innerText = `${activeHabbit.name}`;
+    const newPercent = activeHabbit.days.length / activeHabbit.target > 1 ? 
+    100 : activeHabbit.days.length / activeHabbit.target * 100;
+    page.header.progressPercent.innerText = `${newPercent.toFixed(0)}%`;
+    page.header.progressCoverBar.style = `width: ${newPercent}%`;
+}
+
+function rerenderContent(activeHabbit){
+    if(!activeHabbit){
+        return;
+    }
+    page.content.habbitList.innerHTML = ''
+    for(const habbitTrack in activeHabbit.days){
+        let newDay = document.createElement('div');
+        newDay.classList.add('habbit');
+        newDay.innerHTML = `
+        <div class="habbit__day">День ${Number(habbitTrack) + 1}</div>
+        <div class="habbit__comment">${activeHabbit.days[habbitTrack].comment}</div>
+        <button class="habbit__delete">
+            <img src="/images/delete.svg" alt="Удалить день">
+        </button>
+        `;
+        newDay.querySelector('.habbit__delete').addEventListener('click',()=>{
+            newDay.innerHTML = '';
+            newDay.outerHTML = '';
+            activeHabbit.days.splice(activeHabbit,1)
+            rerenderHeader(activeHabbit)
+        })
+        page.content.habbitList.appendChild(newDay);
+    }
+}
 function rerender(activeHabbitId){
     const activeHabbit = habbits.find((habbit) => habbit.id === activeHabbitId)
     rerenderMenu(activeHabbit);
+    rerenderHeader(activeHabbit)
+    rerenderContent(activeHabbit);
 }
 
         // init // 
